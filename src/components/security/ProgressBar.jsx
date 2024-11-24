ff7e3e216051c9e307abe5c5eb71a529dc3a7747
Lycan-Xx <msbello514@gmail.com>
2024-11-24 20:42:20 +0100

    fixed the progressive bar sign in and sign up 2

--- src/components/security/ProgressBar.jsx
+++ src/components/security/ProgressBar.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 const ProgressBar = ({ currentStep, totalSteps }) => (
   <div className="absolute top-0 left-0 w-full h-1 bg-gray-200 rounded">
     <div
-      className="h-full bg-[#025798] rounded transition-all duration-500"
-      style={{ width: `${(currentStep / totalSteps) * 100}%` }}
+        className="bg-[#025798] h-2.5 rounded-full transition-all duration-500"
+        style={{ width: `${(currentStep / totalSteps) * 100}%` }}
     />
   </div>
 );
